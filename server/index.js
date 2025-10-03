1df2272eccd6158f245865260bdf791a4ad62538
Chauhan Dhruvil <chauhandhruvil12@gmail.com>
2025-10-03 14:10:53 +0530

    CORS

--- server/index.js
+++ server/index.js
@@ -99,7 +99,7 @@ app.use((req, res, next) => {
 });
 
 // Handle preflight for all routes with the same CORS config
-app.options('*', cors({
+app.options('(.*)', cors({
   origin: function (origin, callback) {
     // Allow requests with no origin (like mobile apps or curl requests)
     if (!origin) return callback(null, true);
